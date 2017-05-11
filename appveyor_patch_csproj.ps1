8c8adfb7ede71d4c37f55e21d828fc0db519037b
Karl-Johan Sjogren <karl-johan.sjogren@openlibrarysolutions.com>
2017-05-11 11:46:45 +0200

    Playing around with the build system

--- appveyor_patch_csproj.ps1
+++ appveyor_patch_csproj.ps1
@@ -1,8 +1,7 @@
-if($env:appveyor_repo_branch -eq "master") {
+if($env:appveyor_repo_branch -ne "master") {
     $xmlPath = "$env:appveyor_build_folder\src\SyndicationCore\SyndicationCore.csproj"
     $xml = [xml](get-content $xmlPath)
     $propertyGroup = $xml.Project.PropertyGroup
-
     $propertyGroup.Version = $env:appveyor_build_version
     $xml.Save($xmlPath)
 }
