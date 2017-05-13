05b55cbb1d1f36ac8ffdfabe9cf3760cd0c0d3c8
Karl-Johan Sjogren <karl-johan.sjogren@openlibrarysolutions.com>
2017-05-13 11:51:03 +0200

    Fixes #1

--- src/SyndicationCore/Extensions.cs
+++ src/SyndicationCore/Extensions.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Globalization;
 using System.Xml.Linq;
 
 namespace SyndicationCore {
     public static class Extensions {
+        private static IFormatProvider _formatProvider = new CultureInfo("en-US"); 
+
         public static string GenerateString(this ISyndicationGenerator generator, SyndicationFeed feed) {
-            var doc = generator.Generate(feed);
-            return doc.ToString();
+            return generator.Generate(feed)?.ToString();
         }
 
         public static string ToRFC822(this DateTime? date) {
@@ -17,13 +19,17 @@ namespace SyndicationCore {
 
         public static string ToRFC822(this DateTime date) {
             var offset = TimeZoneInfo.Local.Base​Utc​Offset.Hours;
+            
             var timeZone = "+" + offset.ToString().PadLeft(2, '0');
  
             if(offset < 0) {
                 timeZone = "-" + (offset * -1).ToString().PadLeft(2, '0');
             }
+
+            if(date.Kind == DateTimeKind.Utc)
+                timeZone = "+00";
  
-            return date.ToString("ddd, dd MMM yyyy HH:mm:ss " + timeZone.PadRight(5, '0'));
+            return date.ToString("ddd, dd MMM yyyy HH:mm:ss " + timeZone.PadRight(5, '0'), _formatProvider);
         }
 
         public static string ToRFC3339(this DateTime? date) {
